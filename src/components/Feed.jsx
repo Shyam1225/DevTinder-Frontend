a180c95ad214959fe72d8d3feda231644ac9da65
Shyam1225 <damanihit@gmail.com>
2026-01-08 18:06:43 +0530

    Send/Ignore the user card from the feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -12,19 +12,24 @@ const Feed = () => {
   const getFeed = async () => {
     if (feed) return;
     try {
-      const res = await axios.get(BASE_URL + "/user/feed", {
+      const res = await axios.get(BASE_URL + "/feed", {
         withCredentials: true,
       });
       dispatch(addFeed(res?.data?.data));
-    } catch (e) {
+    } catch (err) {
+      console.error(err);
       //TODO: handle error
-      console.error(e);
     }
   };
 
   useEffect(() => {
     getFeed();
   }, []);
+  if (!feed) return;
+
+  if (feed.length <= 0)
+    return <h1 className="flex justify-center my-10">No new users founds!</h1>;
+
   return (
     feed && (
       <div className="flex justify-center my-10">
--- src/utils/feedSlice.js
+++ src/utils/feedSlice.js
@@ -4,10 +4,15 @@ const feedSlice = createSlice({
   name: "feed",
   initialState: null,
   reducers: {
-    addFeed: (state, action) => {return action.payload;},
-    removeFeed: () => {return null},
+    addFeed: (state, action) => {
+      return action.payload;
+    },
+    removeUserFromFeed: (state, action) => {
+      const newFeed = state.filter((user) => user._id !== action.payload);
+      return newFeed;
+    },
   },
 });
 
+export const { addFeed, removeUserFromFeed } = feedSlice.actions;
 export default feedSlice.reducer;
-export const { addFeed, removeFeed } = feedSlice.actions;
