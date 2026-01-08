f4f72f28fed896d9976a3d7cb7f6b41dcaa135ce
Shyam1225 <damanihit@gmail.com>
2026-01-08 17:41:40 +0530

    feat:connection

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,31 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { removeUser } from "../utils/userSlice";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
+import { removeUser } from "../utils/userSlice";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
-  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
-       await axios.post(BASE_URL + "/logout", {}, {
-        withCredentials: true,
-      });
+      await axios.post(BASE_URL + "/logout", {}, { withCredentials: true });
       dispatch(removeUser());
-      navigate("/login");
+      return navigate("/login");
     } catch (err) {
       console.error(err);
+      // Error logic maybe redirect to error page
     }
   };
 
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <Link to="/" className="btn btn-ghost text-xl">👩‍💻 DevTinder</Link>
+        <Link to="/" className="btn btn-ghost text-xl">
+          👩‍💻 DevTinder
+        </Link>
       </div>
       {user && (
         <div className="flex-none gap-2">
@@ -43,7 +42,7 @@ const NavBar = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
                 <Link to="/profile" className="justify-between">
@@ -52,7 +51,7 @@ const NavBar = () => {
                 </Link>
               </li>
               <li>
-                <a>Settings</a>
+                <Link to="/connections">Connections</Link>
               </li>
               <li>
                 <a onClick={handleLogout}>Logout</a>
