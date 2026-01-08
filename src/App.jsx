6f14143d81e3dde0d2ddcf1b2e6b1bcd003b9553
Shyam1225 <damanihit@gmail.com>
2026-01-08 12:19:24 +0530

    setup redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import React from "react";
-import Navbar from "./Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Body from "./Body";
-import Login from "./Login";
-import Signup from "./Signup";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
 
-const App = () => {
+function App() {
   return (
     <>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Body />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Route>
-      </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
-};
+}
 
 export default App;
